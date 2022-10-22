16b1ac38c856f4b283ffb3c0c89498cd5333fdc9
JKYWHZH <55968062+JKYWHZH@users.noreply.github.com>
2022-10-22 15:47:00 +0800

    git提交测试

--- src/main/java/com/jky/learn/LearnApplication.java
+++ src/main/java/com/jky/learn/LearnApplication.java
@@ -3,6 +3,9 @@ package com.jky.learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 参数验证使用
+ */
 @SpringBootApplication
 public class LearnApplication {
     public static void main(String[] args) {
